03b8e767d9077f1cec6cf9ae3c58ac45a3a4f71c
Ishit Rastogi <ishitrastogi@gmail.com>
2026-06-27 13:12:12 +0530

    update team page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ['image/webp'],
+    formats: ['image/webp', 'image/avif'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.dropbox.com',
+        pathname: '/scl/fi/**',
+      }
+    ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000,
